1290efc5c66aedb3630e100afd885ae264042a99
Gihun Ham <progh2@gmail.com>
2019-12-09 12:54:48 +0900

    #9 3.3.7 클래스형 컴포넌트에서 props 사용하기2 - class 내부에서 기본값 선언(클래스 컴포넌트라면 이쪽이 더 자연스럽다

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름'
+  };
+
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+  };
+
   render() {
     const { name, favoriteNumber, children } = this.props; // 비구조화 할당
     return (
@@ -16,13 +25,5 @@ class MyComponent extends Component {
     );
   }
 }
-MyComponent.defaultProps = {
-  name: '기본 이름'
-};
-
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired
-};
 
 export default MyComponent;
