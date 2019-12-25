e461c23fd902f2dcd8cc9ef0162ef88ef85b38aa
Gihun Ham <progh2@gmail.com>
2019-12-26 00:22:38 +0900

    #10 4.3 함수형 컴포넌트로 구현해 보기 - form 객체 사용, 여러 인풋값 처리

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,14 +1,24 @@
 import React, { useState } from 'react';
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const { username, message } = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value
+    };
+    setForm(nextForm);
+  };
 
   const onClick = () => {
     alert(username + ':' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: ''
+    });
   };
 
   const onKeyPress = e => {
@@ -25,14 +35,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
