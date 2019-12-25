3f08cc16553827f77ed170c64807a7b3ab3c7487
Gihun Ham <progh2@gmail.com>
2019-12-26 00:08:50 +0900

    #10 4.2.5 onKeyPress 이벤트 핸들링

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -20,6 +20,12 @@ export default class EventPractice extends Component {
     });
   };
 
+  handleKeyPress = e => {
+    if (e.key == 'Enter') {
+      this.handleClick();
+    }
+  };
+
   render() {
     return (
       <div>
@@ -37,6 +43,7 @@ export default class EventPractice extends Component {
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
