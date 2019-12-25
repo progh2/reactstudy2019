6b7b14759a2a86e5816390cc681d534cf3e27b14
Gihun Ham <progh2@gmail.com>
2019-12-25 23:59:38 +0900

    #10 4.2.3.1 임의 메서드 만들기 - 기본방식

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,6 +4,26 @@ export default class EventPractice extends Component {
   state = {
     message: ''
   };
+
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handleClick() {
+    alert(this.state.message);
+    this.setState({
+      message: ''
+    });
+  }
+
   render() {
     return (
       <div>
@@ -13,23 +33,9 @@ export default class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={e => {
-            //console.log(e.target.value);
-            this.setState({
-              message: e.target.value
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: ''
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
