75c69b2a70eda90a442c2dca33dbf89785051f33
Gihun Ham <progh2@gmail.com>
2019-12-28 20:38:57 +0900

    #11 5.3.4 컴포넌트에 ref달고 내부 메서드 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import MyComponent from './MyComponent';
-import Counter from './Counter';
-import Say from './Say';
-import EventPractice from './EventPractice';
-import ValidationSample from './ValidationSample';
-import RefSample from './RefSample';
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <br />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
+/*
 const App = () => {
   return (
     <div>
+      <ScrollBox ref={ref => (this.scrollBox = ref)} />
+      <br />
+      <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
+      <hr />
       <RefSample />
       <hr />
       <ValidationSample />
@@ -25,5 +38,5 @@ const App = () => {
     </div>
   );
 };
-
+*/
 export default App;
