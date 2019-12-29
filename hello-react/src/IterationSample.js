d42b76e7f653a379335de2a12fb2eb7fc05b7ec0
Gihun Ham <progh2@gmail.com>
2019-12-29 23:50:41 +0900

    #12 6.2.1 컴포넌트 수정하기

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -2,24 +2,9 @@ import React from 'react';
 
 const IterationSample = () => {
 
-    var numbers = [1, 2, 3, 4, 5];
-    var processed = numbers.map(function (num) {
-        return num * num;
-    });
-    console.log(processed);
-
-    const numbers2 = [1, 2, 3, 4, 5];
-    const result = numbers2.map(num => num * num);
-    console.log(result);
-
-    return (
-        <ul>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
-        </ul>
-    );
+    const names = ['눈사람', '얼음', '눈', '바람'];
+    const nameList = names.map(name => <li>{name}</li>);
+    return <ul>{nameList}</ul>
 };
 
 export default IterationSample;
