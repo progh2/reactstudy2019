fb7acf7044224ba7d7cd2568154a6783c61c603c
Gihun Ham <progh2@gmail.com>
2019-12-29 23:59:48 +0900

    #12 6.4.1 초기 상태 설정하기

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IterationSample = () => {
 
+    const [names, setNames] = useState([
+        {
+            id: 1,
+            text: '눈사람'
+        }, {
+            id: 2,
+            text: '얼음'
+        }, {
+            id: 3,
+            text: '눈'
+        }, {
+            id: 4,
+            text: '바람'
+        }
+    ]);
 
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name, index) => <li key={index}>
-        {name}</li>);
-    return <ul>{nameList}</ul>
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+    const namesList = names.map(name => <li key={
+        name.id
+    }>
+        {
+        name.text
+    }</li>);
+    return <ul>{namesList}</ul>
 };
 
 export default IterationSample;
